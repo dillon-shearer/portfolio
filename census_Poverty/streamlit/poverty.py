57830c4168a8445fd0a377c1d4cabeaca8493f8f
DILLON SHEARER <dillshearer@outlook.com>
2024-01-06 22:53:59 -0500

    update map

--- census_Poverty/streamlit/poverty.py
+++ census_Poverty/streamlit/poverty.py
@@ -66,26 +66,22 @@ def show():
 
     df_county = df[df['REGION_TYPE'] == 'COUNTY']
 
-    # Create the choropleth map
+    # Create the choropleth map for counties
     fig = px.choropleth(
         df_county,
         geojson="https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json",
-        locations='FIPS_CODE',  # DataFrame column with FIPS codes
-        color='MEDIAN_HOUSEHOLD_INCOME',  # DataFrame column with the data to visualize
-        color_continuous_scale='Greens',  # Color scale
-        scope='usa',  # Focus the map on the USA
-        labels={'MEDIAN_HOUSEHOLD_INCOME': 'Median Household Income'},  # Label for color scale
-        title='Median Income (County)'  # Title of the map
+        locations='FIPS_CODE',
+        color='MEDIAN_HOUSEHOLD_INCOME',
+        color_continuous_scale='Greens',
+        scope='usa',
+        labels={'MEDIAN_HOUSEHOLD_INCOME': 'Median Household Income'},
+        title='Median Income (County)'
     )
 
     fig.update_geos(fitbounds="locations")
+    fig.update_layout(margin=dict(l=0, r=0, t=50, b=0), geo_scope='usa')
     
-    # Adjust the layout of the figure for better display
-    fig.update_layout(
-        margin=dict(l=0, r=0, t=50, b=0),  # Reducing the margin for full-width display
-        geo_scope='usa'
-    )
-
+    # Display in Streamlit
     st.plotly_chart(fig, use_container_width=True)
 
     st.dataframe(df_county)
