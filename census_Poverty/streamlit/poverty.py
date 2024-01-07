22b645104d764cbc727259107b2f704b9b87bc4b
DILLON SHEARER <dillshearer@outlook.com>
2024-01-06 22:09:02 -0500

    update page contents

--- census_Poverty/streamlit/poverty.py
+++ census_Poverty/streamlit/poverty.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Main function for the analysis pages
 def show():
     # Set up the page
-    st.title("SAIPE Poverty/Income: Median Income Map")
-    st.write("*This app explores the 2022 SAIPE dataset Median Income interactive map.*")
+    st.title("SAIPE Poverty/Income: Poverty Map")
+    st.write("*This app explores the 2022 SAIPE dataset Poverty interactive map.*")
     st.write("Dillon Shearer - 2024")
     st.write("-----")
