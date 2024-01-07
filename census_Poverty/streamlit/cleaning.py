68556ea5ed927639f237e9355c8fe9489c13bb3a
DILLON SHEARER <dillshearer@outlook.com>
2024-01-06 21:51:56 -0500

    update pages

---
+++ census_Poverty/streamlit/cleaning.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Set page configuration
+st.set_page_config(
+    page_title="SAIPE Data Cleaning/Pre-Processing",
+    page_icon="📊",  # Example: using an emoji as icon
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': None,
+        'Report a Bug': None,  # Removing the 'Report a Bug' option
+        'About': "This app is used for analyzing poverty/median income data."
+    }
+)
+
+# Main function for the analysis pages
+def show():
+    # Set up the page
+    st.title("SAIPE Poverty/Income: Data Cleaning")
+    st.write("Dillon Shearer")
