04b712433deb5a27079135864290871b8298dc7d
Dillon Shearer <dillshearer@outlook.com>
2024-01-06 23:14:36 -0500

    update median income

--- census_Poverty/streamlit/median_income.py
+++ census_Poverty/streamlit/median_income.py
@@ -11,8 +11,8 @@ def load_data():
 # Main function for the analysis pages
 def show():
     # Set up the page
-    st.title("SAIPE Poverty/Income: Poverty Map")
-    st.write("*This app explores the 2022 SAIPE dataset Poverty interactive map.*")
+    st.title("SAIPE Poverty/Income: Median Income Map")
+    st.write("*This app explores the 2022 SAIPE dataset Median Income interactive map.*")
     st.write("Dillon Shearer - 2024")
     st.write("-----")
 
