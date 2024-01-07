8e0034c6fcd8a3886540afffa6ff5f873114bc56
DILLON SHEARER <dillshearer@outlook.com>
2024-01-06 22:09:53 -0500

    update page content

--- census_Poverty/streamlit/home.py
+++ census_Poverty/streamlit/home.py
@@ -3,5 +3,6 @@ import streamlit as st
 # Main function for the analysis pages
 def show():
     # Set up the page
-    st.title("SAIPE Poverty/Income: Data Home")
-    st.write("Dillon Shearer")
+    st.title("SAIPE Poverty/Income: Project Home")
+    st.write("Dillon Shearer - 2024")
+    st.write("------")
